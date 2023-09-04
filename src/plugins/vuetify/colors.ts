817411b5dac86fc1bd750675d521ec89454aafa1
Guilherme Pontes <gui97p.p@gmail.com>
2023-09-03 23:00:27 -0300

    Theme changes

--- src/plugins/vuetify/colors.ts
+++ src/plugins/vuetify/colors.ts
@@ -1,16 +1,16 @@
 export const lightThemeColors = {
   colors: {
-    primary: '#5C5BCD',
-    secondary: '#E1C109',
+    primary: '#405390',
+    secondary: '#907040',
 
     grey: '#E0E0E0',
     lightGrey: '#F5F5F5',
     darkGrey: '#9E9E9E',
 
-    error: '#FF5252',
-    info: '#33B5E5',
-    success: '#4CAF50',
-    warning: '#FFB900',
+    error: '#cd5c5b',
+    info: '#5C5BCD',
+    success: '#5bcd5b',
+    warning: '#cdcd5b',
 
     background: '#F5F5F5',
   },
