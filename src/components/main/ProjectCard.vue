1cea049bf30fb68a98f220d2616f351df6144610
Guilherme Pontes <gui97p.p@gmail.com>
2023-09-06 00:33:49 -0300

    Added project card component

---
+++ src/components/main/ProjectCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card class="card mx-auto">
+    <div class="card__image position-relative">
+      <div class="tags position-absolute d-flex">
+        <div class="chip">PHP</div>
+        <div class="chip">Back-end</div>
+      </div>
+    </div>
+
+    <v-card-title class="mt-5">
+      Project Name - Application
+    </v-card-title>
+
+    <v-card-subtitle class="mt-3 mb-5">
+      Lorem Victrix, cacula, et historia. Lorem Victrix, cacula, et historia
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<style scoped lang="scss">
+.card {
+  &__image {
+    background-color: rgb(var(--v-theme-grey));
+
+    height: 200px;
+    border-radius: inherit;
+
+    .tags {
+      top: 20px;
+      left: 20px;
+    }
+  }
+}
+
+.chip {
+  background-color: white;
+  padding: 5px 10px;
+  border-radius: 15px;
+  color: black;
+  font-size: 1rem;
+  font-weight: bold;
+
+  &:not(:first-child) {
+    margin-left: 10px;
+  }
+}
+</style>
