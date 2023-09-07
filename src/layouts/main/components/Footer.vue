a8b3e2343aa75eab47ac122747607e1d0fc9e112
Guilherme Pontes <gui97p.p@gmail.com>
2023-09-07 15:00:48 -0300

    Refactoring main layout

---
+++ src/layouts/main/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-footer class="bg-tertiary text-center d-flex flex-column mt-10 pa-10 flex-shrink-0">
+    <div class="pt-0">
+      <RouterLink
+          to="/admin"
+          class="text-white text-decoration-none"
+      >
+        Access admin panel
+      </RouterLink>
+    </div>
+
+    <v-divider></v-divider>
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/layouts/main/components/MainContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class="ma-0 pa-0 h-100" fluid>
+    <router-view v-slot="{ Component }">
+      <v-fade-transition hide-on-leave>
+        <div class="h-100" :key="route.name?.toString()">
+          <component :is="Component"/>
+        </div>
+      </v-fade-transition>
+    </router-view>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+
+const route = useRoute()
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
