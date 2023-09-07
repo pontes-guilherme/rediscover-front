86e9cb25f5877189e609bb6b45a4ac9f625a44ba
Guilherme Pontes <gui97p.p@gmail.com>
2023-09-07 15:44:16 -0300

    Fixed footer import

--- src/layouts/main/MainLayout.vue
+++ src/layouts/main/MainLayout.vue
@@ -13,6 +13,7 @@
 <script setup lang="ts">
 import AppBar from "@/layouts/main/components/AppBar.vue";
 import MainContainer from "@/layouts/main/components/MainContainer.vue";
+import Footer from "@/layouts/main/components/Footer.vue";
 </script>
 
 <style scoped lang="scss">
