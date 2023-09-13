c804d5041e9419bf7e56a614996e62c00098511f
Guilherme Pontes <gui97p.p@gmail.com>
2023-09-12 21:56:49 -0300

    Comment input

--- src/views/main/project-details/components/ProjectComments.vue
+++ src/views/main/project-details/components/ProjectComments.vue
@@ -4,8 +4,19 @@
       <Comment :content="comment" v-for="(comment, i) in comments" :key="i"/>
     </div>
 
+    <div class="comment-input">
+      <v-textarea
+        class="mt-5"
+        placeholder="Write a comment..."
+        rows="3"
+        v-model="comment"
+        auto-grow
+        variant="filled"
+      ></v-textarea>
+    </div>
+
     <div class="button mt-5 d-flex align-center justify-center">
-      <v-btn class="btn-add-comment" color="primary">Add comment</v-btn>
+      <v-btn class="btn-add-comment px-16" color="primary">Send</v-btn>
     </div>
   </v-card>
 </template>
@@ -13,6 +24,8 @@
 <script setup lang="ts">
 import Comment from "@/components/main/Comment.vue";
 import {ref} from "vue";
+
+const comment = ref<string>('');
 
 const comments = ref<string[]>([
   'Lorem ipsum dolor sit amet consectetur adipiscing eli0t Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet',
