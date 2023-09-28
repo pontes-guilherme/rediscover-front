8d6c3bc88906c5718d39fc4927156f05c0fae4e7
Guilherme Pontes <gui97p.p@gmail.com>
2023-09-27 22:08:35 -0300

    Fix when loading admin auth state

--- src/store/pages/admin/auth/auth.store.ts
+++ src/store/pages/admin/auth/auth.store.ts
@@ -5,68 +5,71 @@ import {ADMIN_AUTH_KEY, ADMIN_TOKEN_KEY, ADMIN_USER_KEY} from "@/constants/local
 import {User} from "@/models/User.model";
 
 interface LoginResponse {
-    user: User;
-    token: string;
+  user: User;
+  token: string;
 }
 
 export const useAuthStore = defineStore("auth", () => {
-    const isAuthenticated = ref(localStorage.getItem(ADMIN_AUTH_KEY) === 'true' || false);
-    const user = ref<User>(JSON.parse(localStorage.getItem(ADMIN_USER_KEY) || '') || null);
-    const token = ref(localStorage.getItem(ADMIN_TOKEN_KEY) || null);
+  const isAuthenticated = ref(localStorage.getItem(ADMIN_AUTH_KEY) === 'true' || false);
+  const user = ref<User>(
+    JSON.parse(localStorage.getItem(ADMIN_USER_KEY) || '{}')
+    || null
+  );
+  const token = ref(localStorage.getItem(ADMIN_TOKEN_KEY) || null);
 
-    const email = ref('');
-    const password = ref('');
-    const formValid = ref(false);
+  const email = ref('');
+  const password = ref('');
+  const formValid = ref(false);
 
-    function setIsAuthenticated(authenticated: boolean) {
-        isAuthenticated.value = authenticated;
-        localStorage.setItem(ADMIN_AUTH_KEY, authenticated.toString());
-    }
+  function setIsAuthenticated(authenticated: boolean) {
+    isAuthenticated.value = authenticated;
+    localStorage.setItem(ADMIN_AUTH_KEY, authenticated.toString());
+  }
 
-    function setUser(userObject: any) {
-        user.value = userObject;
-        localStorage.setItem(ADMIN_USER_KEY, JSON.stringify(userObject));
-    }
+  function setUser(userObject: any) {
+    user.value = userObject;
+    localStorage.setItem(ADMIN_USER_KEY, JSON.stringify(userObject));
+  }
 
-    function setToken(tokenString: any) {
-        token.value = tokenString;
-        localStorage.setItem(ADMIN_TOKEN_KEY, tokenString);
+  function setToken(tokenString: any) {
+    token.value = tokenString;
+    localStorage.setItem(ADMIN_TOKEN_KEY, tokenString);
 
-    }
+  }
 
-    function setAuthenticatedUser(data: LoginResponse) {
-        setIsAuthenticated(true);
-        setUser(data.user);
-        setToken(data.token);
-    }
+  function setAuthenticatedUser(data: LoginResponse) {
+    setIsAuthenticated(true);
+    setUser(data.user);
+    setToken(data.token);
+  }
 
-    function login() {
-        AuthService
-            .login(email.value, password.value)
-    }
+  function login() {
+    AuthService
+      .login(email.value, password.value)
+  }
 
-    async function logout() {
-        return new Promise((resolve) => {
-            setIsAuthenticated(false);
-            setUser(null);
-            setToken(null);
+  async function logout() {
+    return new Promise((resolve) => {
+      setIsAuthenticated(false);
+      setUser(null);
+      setToken(null);
 
-            resolve(true);
-        });
-    }
+      resolve(true);
+    });
+  }
 
-    return {
-        isAuthenticated,
-        user,
-        token,
-        email,
-        password,
-        formValid,
+  return {
+    isAuthenticated,
+    user,
+    token,
+    email,
+    password,
+    formValid,
 
-        login,
-        logout,
-        setIsAuthenticated,
-        setAuthenticatedUser,
-        setUser,
-    }
+    login,
+    logout,
+    setIsAuthenticated,
+    setAuthenticatedUser,
+    setUser,
+  }
 })
