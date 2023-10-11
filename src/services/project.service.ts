2682b984175f7eadc960aed41a7c30aa280ece30
Guilherme Pontes <gui97p.p@gmail.com>
2023-10-10 23:41:21 -0300

    Added fetch and get to project service

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -1,7 +1,16 @@
 import api from "@/services/client-axios";
-import {CreateProjectRequest} from "@/models/Project.model";
+import {CreateProjectRequest, Project} from "@/models/Project.model";
 import {CustomResponse} from "@/models/Base/CustomResponse.interface";
+import {JsonResponse} from "@/models/Base/JsonResponse.interface";
 
 export const create = async (data: CreateProjectRequest) => {
-    return api.post<CustomResponse<any>>("/projects", data);
+  return api.post<CustomResponse<any>>("/projects", data);
+}
+
+export const fetch = async () => {
+  return api.get<JsonResponse<Project[]>>("/projects");
+}
+
+export const get = async (id: number) => {
+  return api.get<JsonResponse<Project>>(`/projects/${id}`);
 }
